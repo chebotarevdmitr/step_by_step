8b0fa451df964ca809172d17c9affc7628f26a34
DIM <4ebotarov_dim@inbox.ru>
2024-11-17 20:04:19 +0300

    teorema Pifagora

--- step_by_step.cpp
+++ step_by_step.cpp
@@ -1,28 +1,24 @@
-﻿// step_1
+﻿// step 2 
+//tip s plavaushey nochkoy  -> float -> double 
+//ispolzovanie teoremi Pifagora dlya nahojdeniya dlinny gipotenuzy
+//pryamougolnogo treugolnika pri zadannyh dlinnah ego katetah
+
+/* 
+  Теорема Пифагора - это фундаментальная теорема в геометрии, которая гласит, 
+  что в прямоугольном треугольнике квадрат гипотенузы равен сумме квадратов катетов.
+*/
+
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 int main()
 {
-    // демонстрация различия между целыми числами 
-    // со знаком и бес знака
-    short int i;            // короткое целое со знаком
-    short unsigned int j;  // короткое целое без знака
-
-    j = 60000;  //60000 находится в допустимом значении для unsignet short int но как правило будет
-    i = j;     //вне допустимого диапазон длч числа signed short int в результате присваивания его
-              //переменной i оно будет интропретироваться как отрицательное число
-
-     cout << i << " " << j << endl; //вывод в консоль -5536 60000
-  
-     char ch;
-     ch = 'X'; //  символьной переменной char присвоили значение " X "
-     cout << " \t This is char ch = " << ch << endl; //Вывод :  This is char ch = Х
-
-     cout << "\tThe program outputs the Latin alphabet" << endl;
-     cout << "\n";
-     char letter;
-     for (letter = 'A'; letter <= 'Z'; letter++)    //переменная типа char для 
-         cout <<     letter << " ";                 //управления циклом for
+    double x, y, z;
+    x = 5.0;
+    y = 4.0;
+    z = sqrt(x * x + y * y);
+    cout << "Gipotenuza =" << z;
 
     system("pause>0");
 }
