31056fc8bc269f5588ad4f62bc160ea4c45dbdd2
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2024-12-23 23:01:56 +0300

    addad  bool a c

--- XOR.cpp
+++ XOR.cpp
@@ -20,5 +20,12 @@ int main() {
    q = false;
    cout << p << " XOR " << q << " ravno " << ((p || q) && !(p && q)) << "\n";
 
-   return 0;
+   
+    bool a = true;
+    bool c = false;
+
+    cout << "a = " << a << endl; // выводит: a = 1
+    cout << "b = " << c << endl; // выводит: b = 0
+
+    return 0;
 }
