2c5dfa489048ab48b1194a17c1b269593df245aa
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-03-09 20:57:55 +0300

    added tast

--- test_preobraz.cpp
+++ test_preobraz.cpp
@@ -2,6 +2,8 @@
 using namespace std;
  int main(int argc, const char** argv) {
     int x;
-
+    for (int i = 1; i <= 10; i++) {
+      cout<<i<<"/2 ravno: " <<(float)i/2<<endl;//приведение к типу float для получения дробного числа
+    }
     return 0;
 }
